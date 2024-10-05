db6f4c98f6c766b8ba038dd7e7c2a231c84c416f
Shubham Kumar <imshubhamkr50@gmail.com>
2024-10-05 12:50:43 +0530

    Create service & repository to add & fetch jobs

--- src/main/java/com/example/JobApp/controller/JobController.java
+++ src/main/java/com/example/JobApp/controller/JobController.java
@@ -1,12 +1,16 @@
 package com.example.JobApp.controller;
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
+    @Autowired
+    private JobService jobService;
 
     @GetMapping({"/","home"})
     public String home(){
@@ -20,6 +24,7 @@ public class JobController {
 
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost){
+        jobService.addJob(jobPost);
         return "success";
     }
 }
